a9d849a1419d88d56755c11caf92a88ef20c22dc
Kate Shaffer <kate.shaffer@gmail.com>
2017-01-17 18:07:28 -0800

    Updated _getActionsFromAPI() request to include auth info

--- app/components/WelcomeView/WelcomeView.js
+++ app/components/WelcomeView/WelcomeView.js
@@ -5,10 +5,11 @@ import {
   Button,
   Alert
 } from 'react-native';
+import base64 from 'base-64';
 
-import styles from '../../styles'
-
-import AlertListView from '../AlertListView/AlertListView'
+import Environment from '../../config/environment';
+import styles from '../../styles';
+import AlertListView from '../AlertListView/AlertListView';
 
 export default class WelcomeView extends Component {
   // // @TODO - remove -- this was for hard-coded list
@@ -19,14 +20,23 @@ export default class WelcomeView extends Component {
   //   });
   // };
 
+
   _getActionsFromAPI = () => {
-    fetch('http://localhost:5000/api/v1.0/actions')
+    const req_object = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Basic '+ base64.encode(Environment.username + ':' + Environment.password)
+      },
+    };
+    // console.log(Environment.username + ':' + Environment.password); // NOTE: log
+
+    fetch('http://localhost:5000/api/v1.0/actions', req_object)
     .then((response) => response.json())
     .then((responseData) => {
       // Alert.alert(
       //   "GET Response",
       //   "Data: " + responseData.call_actions[0]["headline"]
-      // )
+      // ) // NOTE: log
       this._loadActionList(responseData)
     })
     .catch((error) => {
