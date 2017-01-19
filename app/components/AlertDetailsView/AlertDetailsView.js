491afcc54dd4c97d15e014f18c853516029506cb
Kate Shaffer <kate.shaffer@gmail.com>
2017-01-19 12:32:00 -0800

    Normalized phone number for phonecall test

--- app/components/AlertDetailsView/AlertDetailsView.js
+++ app/components/AlertDetailsView/AlertDetailsView.js
@@ -11,7 +11,10 @@ import { phonecall, email } from 'react-native-communications';
 export default class AlertDetailsView extends Component {
   constructor(props) {
     super(props);
-    this.actionData = this.props.actionData
+    this.actionData = this.props.actionData;
+    if (this.props.actionData['target_phone_number'] !== undefined && this.props.actionData['target_phone_number'] !== null) {
+      this.phone_number = '1' + this.props.actionData['target_phone_number']
+    }
   }
 
   render() {
@@ -26,9 +29,9 @@ export default class AlertDetailsView extends Component {
         <Text style={styles.title}>
           {this.actionData['title']}
         </Text>
-        <TouchableHighlight onPress={() => phonecall(this.actionData['target_phone_number'], false)}>
+        <TouchableHighlight onPress={() => phonecall(this.phone_number, false)}>
           <Text style={styles.steelBlue}>
-            {this.actionData['target_phone_number']}
+            {this.phone_number}
           </Text>
         </TouchableHighlight>
         <Text style={styles.steelBlue}>
